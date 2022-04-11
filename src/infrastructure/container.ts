09f79c9f875e5093effcd577e7ebf8908bd956f2
Hamid Gasmi <mamid1706@hotmail.fr>
2022-04-11 04:46:44 -0700

    add use case factory to DI

--- src/infrastructure/container.ts
+++ src/infrastructure/container.ts
@@ -1,10 +1,12 @@
 import { createContainer, AwilixContainer, asValue, asClass } from 'awilix'
+import { UseCaseFactory } from '../application/common/useCasesFactory'
 
 export function configureContainer (): AwilixContainer {
   return createContainer()
     .register({
-        //todo: to complete
-        // applicationCache: asValue(applicationCache), // asValue because it's an application cache
-        // permissionService: asClass(PermissionService).scoped()
+      useCaseFactory: asClass(UseCaseFactory).scoped()
+      //todo: to complete
+      // applicationCache: asValue(applicationCache), // asValue because it's an application cache
+      // permissionService: asClass(PermissionService).scoped()
     })
 }
