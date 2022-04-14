0b448ae9e1d40636c34795c478f9c51de5e64f77
Hamid Gasmi <mamid1706@hotmail.fr>
2022-04-14 16:34:28 -0700

    add guard class

---
+++ src/shared/core/guard.ts
@@ -0,0 +1,40 @@
+import { Result } from './result'
+
+export interface IGuardArgument {
+  argument: any
+  argumentName: string
+}
+
+export type GuardArgumentCollection = IGuardArgument[]
+
+/*
+ * //todo: what if I use joi package here? 
+ *     - Domain value-object / entity will depend on it? 
+ *     - I should use Dependency Inversion Principle to not break clean architecture
+**/
+export class Guard {
+
+  public static greaterThan (minValue: number, actualValue: number): Result<boolean> {
+    return actualValue > minValue
+      ? Result.success(true)
+      : Result.failure(`Number given {${actualValue}} is not greater than {${minValue}}`)
+  }
+
+  public static againstAtLeast (numChars: number, text: string): Result<boolean> {
+    return text.length >= numChars
+      ? Result.success(true)
+      : Result.failure(`Text is not at least ${numChars} chars.`)
+  }
+
+  public static againstAtMost (numChars: number, text: string): Result<boolean> {
+    return text.length <= numChars
+      ? Result.success(true)
+      : Result.failure(`Text is greater than ${numChars} chars.`)
+  }
+
+  public static againstNullOrUndefined (argument: any, argumentName: string): Result<boolean> {
+    return (argument === null || argument === undefined)
+      ? Result.failure(`${argumentName} is null or undefined`)
+      : Result.success(true)
+  }
+}
